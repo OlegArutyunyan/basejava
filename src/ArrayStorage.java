8c291b60fd89e8110d1659b9eb3a5cd359002852
OlegArutyunyan <oleg.arl@yandex.ru>
2022-01-18 19:31:40 +0300

    Finalized new array handle method

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -8,70 +8,43 @@ public class ArrayStorage {
     public int array_size = 0;
 
     void clear() {
-        for (int i = 0; i <= storage.length; i++) {
-            if (storage[i] != null) {
-                storage[i] = null;
-            } else {
-                break;
-            }
+        for (int i = 0; i < array_size; i++) {
+            storage[i] = null;
         }
         array_size = 0;
     }
 
     void save(Resume r) {
-        for (int i = 0; i <= storage.length; i++) {
-            if (storage[i] == null) {
-                storage[i] = r;
-                array_size++;
-                break;
-            }
-        }
+        storage[array_size] = r;
+        array_size++;
     }
-    //hweiuhfoiurhgipuohwropig
 
     Resume get(String uuid) {
-        for (Resume n : storage) {
-            if (n == null) {
-                break;
-            } else if (n.toString().equals(uuid)) {
-                return n;
+        for (int i = 0; i < array_size; i++) {
+            if (storage[i].toString().equals(uuid)) {
+                return storage[i];
             }
         }
         return null;
     }
 
     void delete(String uuid) {
-        int i = 0;
-        for (Resume n : storage) {
-            if (n == null) {
-                break;
-            } else if (n.toString().equals(uuid)) {
-                for (int j = i; j <= 10000; j++) {
+        for (int i = 0; i < array_size; i++) {
+            if (storage[i].toString().equals(uuid)) {
+                 for (int j = i; j < array_size; j++) {
                     storage[j] = storage[j + 1];
-                    if (storage[j + 1] == null) {
-                        break;
-                    }
                 }
-                array_size--;
                 break;
             }
-            i++;
         }
+        array_size--;
     }
 
     /**
      * @return array, contains only Resumes in storage (without null)
      */
     Resume[] getAll() {
-        int i = 0;
-        for (Resume n : storage) {
-            if (n != null) {
-                i++;
-            } else {
-                break;
-            }
-        }
-        return Arrays.copyOfRange(storage, 0, i);
+        return Arrays.copyOfRange(storage, 0, array_size);
     }
 
     int size() {
