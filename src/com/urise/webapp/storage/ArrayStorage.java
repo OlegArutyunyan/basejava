d76305761f525fbdd28b51bbfee0d1c19d03bdc7
OlegArutyunyan <oleg.arl@yandex.ru>
2022-01-25 18:09:13 +0300

    Prepared to lesson3

--- src/com/urise/webapp/storage/ArrayStorage.java
+++ src/com/urise/webapp/storage/ArrayStorage.java
@@ -7,20 +7,7 @@ import java.util.Arrays;
 /**
  * Array based storage for Resumes
  */
-public class ArrayStorage {
-    private final Resume[] storage = new Resume[10000];
-    private int size = 0;
-
-    /* Attempt to create additional method to avoid use of duplicate code
-     * Method is used to check if the resume is existing. */
-    private int findIndex(String uuid) {
-        for (int i = 0; i < size; i++) {
-            if (uuid == storage[i].getUuid()) {
-                return i;
-            }
-        }
-        return -1;
-    }
+public class ArrayStorage extends AbstractArrayStorage {
 
     public void clear() {
         Arrays.fill(storage, 0, size, null);
@@ -28,58 +15,46 @@ public class ArrayStorage {
     }
 
     public void update(Resume r) {
-        int index = findIndex(r.getUuid());
-
-        if (index >= 0) {
-            storage[index] = r;
+        int index = getIndex(r.getUuid());
+        if (index == -1) {
+            System.out.println("Resume " + r.getUuid() + " not exist");
         } else {
-            System.out.println("No resume " + r.getUuid() + " found!");
+            storage[index] = r;
         }
     }
 
     public void save(Resume r) {
-        int index = findIndex(r.getUuid());
-
-        if (size >= storage.length) {
-            System.out.println("Storage is full!");
-        } else if (index == -1) {
+        if (getIndex(r.getUuid()) != -1) {
+            System.out.println("Resume " + r.getUuid() + " already exist");
+        } else if (size >= STORAGE_LIMIT) {
+            System.out.println("Storage overflow");
+        } else {
             storage[size] = r;
             size++;
-        } else {
-            System.out.println("Resume " + r.getUuid() + " already exists!");
         }
-    }
-
-    public Resume get(String uuid) {
-        int index = findIndex(uuid);
-
-        if (index >= 0) {
-            return storage[index];
-        }
-        System.out.println("No resume " + uuid + " found!");
-        return null;
     }
 
     public void delete(String uuid) {
-        int index = findIndex(uuid);
-
-        if (index >= 0) {
-            System.arraycopy(storage, index + 1, storage, index, size - 1 - index);
+        int index = getIndex(uuid);
+        if (index == -1) {
+            System.out.println("Resume " + uuid + " not exist");
+        } else {
+            storage[index] = storage[size - 1];
             storage[size - 1] = null;
             size--;
-        } else {
-            System.out.println("No resume " + uuid + " found. Nothing to delete!");
         }
     }
 
-    /**
-     * @return array, contains only Resumes in storage (without null)
-     */
     public Resume[] getAll() {
-        return Arrays.copyOf(storage, size);
+        return Arrays.copyOfRange(storage, 0, size);
     }
 
-    public int size() {
-        return size;
+    protected int getIndex(String uuid) {
+        for (int i = 0; i < size; i++) {
+            if (uuid.equals(storage[i].getUuid())) {
+                return i;
+            }
+        }
+        return -1;
     }
 }
