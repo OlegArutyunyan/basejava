15223e4f6a3253416cd11f2cbc9dfcc55db0fdad
OlegArutyunyan <oleg.arl@yandex.ru>
2022-03-31 11:15:24 +0300

    HW12 fixed realization

--- src/com/urise/webapp/Streams.java
+++ src/com/urise/webapp/Streams.java
@@ -6,28 +6,23 @@ import java.util.stream.Collectors;
 
 public class Streams {
     private static final int[] ARRAY_OF_NUMBERS = {1, 6, 8, 1, 5, 3, 3, 6, 5};
+    private static final List<Integer> listOfNumbers =
+            Arrays.stream(ARRAY_OF_NUMBERS).boxed().distinct().collect(Collectors.toList());
 
     public static void main(String[] args) {
-        System.out.println("First task: " + minValue(ARRAY_OF_NUMBERS));
-    }
-
-    private static int minValue(int[] values) {
-        int result = 0;
-        List<Integer> listOfNumbers = Arrays.stream(values).boxed().distinct().sorted().collect(Collectors.toList());
+        System.out.println("First task: " + minValue());
         System.out.println("Second task:");
-        oddOrEven(listOfNumbers).forEach(System.out::print);
-        System.out.println();
-
-        for (Integer number : listOfNumbers) {
-            result = result * 10 + number;
-        }
-        return result;
+        oddOrEven().forEach(System.out::print);
     }
 
-    private static List<Integer> oddOrEven(List<Integer> integers) {
-        if (integers.stream().reduce(0, Integer::sum) % 2 == 0) {
-            return integers.stream().filter(i -> i % 2 == 1).collect(Collectors.toList());
-        }
-        return integers.stream().filter(i -> i % 2 == 0).collect(Collectors.toList());
+    private static int minValue() {
+        return Arrays.stream(Streams.ARRAY_OF_NUMBERS).distinct().sorted()
+                .reduce(0, (partialResult, currentNumber) -> partialResult * 10 + currentNumber);
+    }
+
+    private static List<Integer> oddOrEven() {
+        int sum = Streams.listOfNumbers.stream().reduce(0, Integer::sum);
+
+        return Streams.listOfNumbers.stream().filter(i -> i % 2 != sum % 2).collect(Collectors.toList());
     }
 }
