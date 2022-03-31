0a0b8dfe3eb71ebf740864e7a220d1114bb6fd10
OlegArutyunyan <oleg.arl@yandex.ru>
2022-03-31 12:42:05 +0300

    HW12 returned signature for methods

--- src/com/urise/webapp/Streams.java
+++ src/com/urise/webapp/Streams.java
@@ -5,24 +5,25 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 public class Streams {
-    private static final int[] ARRAY_OF_NUMBERS = {1, 6, 8, 1, 5, 3, 3, 6, 5};
-    private static final List<Integer> listOfNumbers =
-            Arrays.stream(ARRAY_OF_NUMBERS).boxed().distinct().collect(Collectors.toList());
 
     public static void main(String[] args) {
-        System.out.println("First task: " + minValue());
+        final int[] ARRAY_OF_NUMBERS = {1, 6, 8, 1, 5, 3, 3, 6, 5};
+        final List<Integer> listOfNumbers =
+                Arrays.stream(ARRAY_OF_NUMBERS).boxed().distinct().collect(Collectors.toList());
+
+        System.out.println("First task: " + minValue(ARRAY_OF_NUMBERS));
         System.out.println("Second task:");
-        oddOrEven().forEach(System.out::print);
+        oddOrEven(listOfNumbers).forEach(System.out::print);
     }
 
-    private static int minValue() {
-        return Arrays.stream(Streams.ARRAY_OF_NUMBERS).distinct().sorted()
+    private static int minValue(int[] arrayOfNumbers) {
+        return Arrays.stream(arrayOfNumbers).distinct().sorted()
                 .reduce(0, (partialResult, currentNumber) -> partialResult * 10 + currentNumber);
     }
 
-    private static List<Integer> oddOrEven() {
-        int sum = Streams.listOfNumbers.stream().reduce(0, Integer::sum);
+    private static List<Integer> oddOrEven(List<Integer> listOfNumbers) {
+        int sum = listOfNumbers.stream().reduce(0, Integer::sum);
 
-        return Streams.listOfNumbers.stream().filter(i -> i % 2 != sum % 2).collect(Collectors.toList());
+        return listOfNumbers.stream().filter(i -> i % 2 != sum % 2).collect(Collectors.toList());
     }
 }
